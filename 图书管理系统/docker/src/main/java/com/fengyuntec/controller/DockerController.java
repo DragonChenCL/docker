9409abeff65908cd935e0b74fa57ae19775e584e
DragonChenCL <chenlong_qrr@163.com>
2018-12-27 16:24:03 +0800

    docker测试项目

---
+++ 图书管理系统/docker/src/main/java/com/fengyuntec/controller/DockerController.java
@@ -0,0 +1,13 @@
+package com.fengyuntec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @GetMapping("/hello")
+    public String docker(){
+        return "Hello docker";
+    }
+}
